d6478f4f429e48cfa26ee9f5d656d14927aba207
“Philip” <philip@randomsmiths.com>
2021-10-22 11:11:26 -0400

    Got javaFXworking

---
+++ HauntedHouse/src/Player.java
@@ -0,0 +1,23 @@
+public class Player
+{
+   int health = 3;
+   int maxhealth = 3;
+   boolean collectedBlaster = false;
+   boolean dogToy = true;
+   boolean vaderLightsaber = false;
+   boolean trooperArmor = false;
+   boolean hasKey = false;
+   boolean hasWrench = false;
+   boolean lukeLightsaber = false;
+   boolean trained = false;
+   boolean hasLadder = false;
+   String wisdom;
+   public void gainArmor(){
+       trooperArmor = true;
+       maxhealth = 5;
+   }
+   
+   
+   
+}
+
